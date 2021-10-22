aa9b661fe231a62d6bef0b373ffd93a3d8ae0bfe
Matheus <matth11@gmail.com.br>
2021-10-22 11:56:35 -0300

    adição ao paciente

--- src/main/java/br/com/matheus/clinicaapi/repository/DadosRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.matheus.clinicaapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.matheus.clinicaapi.model.Cliente;
-
-public interface DadosRepository extends JpaRepository<Cliente, Long>{
-	
-}
